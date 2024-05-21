4c9f2644d6bb1d1d9ef7b53099d7c8d213f39cea
Angelo Convento <convento.angelo@gmail.com>
2024-05-22 01:56:57 +0800

    Working AuthJS implementation

---
+++ svelte-app/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { SignIn, SignOut } from '@auth/sveltekit/components';
+	import { Button } from '$lib/components/ui/button';
+    import { page } from '$app/stores';
+</script>
+
+<div>
+	<p>SmartStash</p>
+    {#if $page.data.session?.user}
+        <p>Welcome, {$page.data.session.user.name}!</p>
+        <SignOut>
+            <Button slot="submitButton" class="buttonPrimary">Sign out</Button>
+        </SignOut>
+    {:else}
+        <p>Sign in to continue</p>
+        <SignIn>
+            <Button slot="submitButton" class="buttonPrimary">Sign in</Button>
+        </SignIn>
+    {/if}
+</div>
