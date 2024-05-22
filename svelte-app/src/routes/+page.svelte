63001d18ca05d0c3f8145859908e3b383720587d
Angelo Convento <convento.angelo@gmail.com>
2024-05-23 02:25:39 +0800

    chore: Fixed redirects and added dashboard page

--- svelte-app/src/routes/+page.svelte
+++ svelte-app/src/routes/+page.svelte
@@ -1,21 +1,32 @@
 <script lang="ts">
 	import { SignIn, SignOut } from '@auth/sveltekit/components';
 	import { Button } from '$lib/components/ui/button';
-    import { page } from '$app/stores';
+	import { page } from '$app/stores';
 </script>
 
-<div>
-	<p>SmartStash</p>
-    {#if $page.data.session?.user}
-        <p>Welcome, {$page.data.session.user.name}!</p>
-        <p>Your ID is {$page.data.session.user.id}.</p>
-        <SignOut>
-            <Button slot="submitButton" class="buttonPrimary">Sign out</Button>
-        </SignOut>
-    {:else}
-        <p>Sign in to continue</p>
-        <SignIn>
-            <Button slot="submitButton" class="buttonPrimary">Sign in</Button>
-        </SignIn>
-    {/if}
-</div>
+<main class="flex w-full h-screen justify-center flex-col items-center">
+	<div class="flex w-full max-w-sm flex-col items-center mb-16">
+		<h1
+			class="hidden text-center text-3xl font-bold leading-tight tracking-tighter md:block md:text-6xl lg:leading-[1.1] py-4"
+		>
+			SmartStash
+		</h1>
+		{#if $page.data.session?.user}
+			<Button variant="outline" href="/dashboard" class="my-2">Go to Dashboard</Button>
+			<SignOut>
+				<Button slot="submitButton" class="buttonPrimary">Sign out</Button>
+			</SignOut>
+		{:else}
+			<p class="py-2">Sign in to continue</p>
+			<SignIn
+				options={{
+					redirectTo: $page.data.redirectTo
+						? `/${decodeURIComponent($page.data.redirectTo).slice(1)}`
+						: `/dashboard`
+				}}
+			>
+				<Button slot="submitButton">Sign in</Button>
+			</SignIn>
+		{/if}
+	</div>
+</main>
---
+++ svelte-app/src/routes/dashboard/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { SignOut } from '@auth/sveltekit/components';
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<div>
+	<p>Welcome, {$page.data.session?.user?.name}!</p>
+	<p>Your ID is {$page.data.session?.user?.id}.</p>
+	<SignOut options={{ redirectTo: '/' }}>
+		<Button slot="submitButton">Sign out</Button>
+	</SignOut>
+</div>
