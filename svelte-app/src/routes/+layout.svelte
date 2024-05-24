a90b6289777eefeeb3eacf51f4cb627ae4de99e8
Angelo Convento <convento.angelo@gmail.com>
2024-05-25 01:19:01 +0800

    chore: Added dark mode and switcher

--- svelte-app/src/routes/+layout.svelte
+++ svelte-app/src/routes/+layout.svelte
@@ -14,50 +14,75 @@
 	import Settings from 'lucide-svelte/icons/settings';
 	import ShoppingCart from 'lucide-svelte/icons/shopping-cart';
 	import { SignOut } from '@auth/sveltekit/components';
+	import { ModeWatcher } from 'mode-watcher';
+	import Sun from 'lucide-svelte/icons/sun';
+	import Moon from 'lucide-svelte/icons/moon';
+	import { resetMode, setMode } from 'mode-watcher';
 </script>
 
+<ModeWatcher />
 {#if $page.data.session?.user && $page.route.id !== '/'}
-	<div class="flex min-h-screen w-full flex-col bg-muted/40 sm:flex-row">
-		<aside class="fixed inset-y-0 left-0 z-10 hidden w-56 flex-col border-r bg-background sm:flex">
+	<div class="flex min-h-screen w-full flex-col bg-muted/40">
+		<aside class="fixed inset-y-0 left-0 z-10 hidden w-64 flex-col border-r bg-background sm:flex">
 			<nav class="flex flex-col items-start gap-2 px-2 sm:py-5">
-				<DropdownMenu.Root>
-					<DropdownMenu.Trigger asChild let:builder>
-						<Button
-							variant="ghost"
-							class="mb-2 h-auto w-full items-start justify-start"
-							builders={[builder]}
-						>
-							<div class="flex flex-row items-center gap-2">
-								<Avatar.Root class="h-8 w-8">
-									<Avatar.Image src={$page.data.session.user.image} />
-									<Avatar.Fallback
-										>{$page.data.session?.user?.name[0]}{$page.data.session?.user?.name
-											.split(' ')
-											.pop()?.[0]}</Avatar.Fallback
-									>
-								</Avatar.Root>
-								<p class="text-lg font-bold tracking-tight">
-									{$page.data.session?.user?.name.split(' ')[0]}
-								</p>
-							</div>
-						</Button>
-					</DropdownMenu.Trigger>
-					<DropdownMenu.Content align="start" class="w-52">
-						<DropdownMenu.Label
-							><p>Welcome, {$page.data.session?.user?.name}!</p></DropdownMenu.Label
-						>
-						<DropdownMenu.Separator />
-						<a href="/app/settings">
-							<DropdownMenu.Item class="cursor-pointer">Settings</DropdownMenu.Item></a
-						>
-						<DropdownMenu.Separator />
-						<DropdownMenu.Item>
-							<SignOut options={{ redirectTo: '/' }}>
-								<div slot="submitButton" class="flex w-48 items-start">Sign out</div>
-							</SignOut>
-						</DropdownMenu.Item>
-					</DropdownMenu.Content>
-				</DropdownMenu.Root>
+				<div class="mb-2 flex flex-row items-center justify-between w-full">
+					<DropdownMenu.Root>
+						<DropdownMenu.Trigger asChild let:builder>
+							<Button
+								variant="ghost"
+								class="h-auto items-start justify-start"
+								builders={[builder]}
+							>
+								<div class="flex flex-row items-center gap-2">
+									<Avatar.Root class="h-8 w-8">
+										<Avatar.Image src={$page.data.session.user.image} />
+										<Avatar.Fallback
+											>{$page.data.session?.user?.name[0]}{$page.data.session?.user?.name
+												.split(' ')
+												.pop()?.[0]}</Avatar.Fallback
+										>
+									</Avatar.Root>
+									<p class="text-lg font-bold tracking-tight">
+										{$page.data.session?.user?.name.split(' ')[0]}
+									</p>
+								</div>
+							</Button>
+						</DropdownMenu.Trigger>
+						<DropdownMenu.Content align="start" class="w-60">
+							<DropdownMenu.Label
+								><p>Welcome, {$page.data.session?.user?.name}!</p></DropdownMenu.Label
+							>
+							<DropdownMenu.Separator />
+							<a href="/app/settings">
+								<DropdownMenu.Item class="cursor-pointer">Settings</DropdownMenu.Item></a
+							>
+							<DropdownMenu.Separator />
+							<DropdownMenu.Item>
+								<SignOut options={{ redirectTo: '/' }}>
+									<div slot="submitButton" class="flex w-56 items-start">Sign out</div>
+								</SignOut>
+							</DropdownMenu.Item>
+						</DropdownMenu.Content>
+					</DropdownMenu.Root>
+					<DropdownMenu.Root>
+						<DropdownMenu.Trigger asChild let:builder>
+							<Button builders={[builder]} variant="ghost" size="icon">
+								<Sun
+									class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+								/>
+								<Moon
+									class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+								/>
+								<span class="sr-only">Toggle theme</span>
+							</Button>
+						</DropdownMenu.Trigger>
+						<DropdownMenu.Content align="end">
+							<DropdownMenu.Item on:click={() => setMode('light')}>Light</DropdownMenu.Item>
+							<DropdownMenu.Item on:click={() => setMode('dark')}>Dark</DropdownMenu.Item>
+							<DropdownMenu.Item on:click={() => resetMode()}>System</DropdownMenu.Item>
+						</DropdownMenu.Content>
+					</DropdownMenu.Root>
+				</div>
 				<div
 					class="colors flex w-full justify-start rounded-lg py-2 text-muted-foreground transition-colors hover:bg-accent hover:text-foreground {$page.route.id?.startsWith(
 						'/app/dashboard'
@@ -110,7 +135,7 @@
 				</div>
 			</nav>
 		</aside>
-		<div class="flex w-full flex-col sm:gap-4 sm:py-14 sm:pl-56">
+		<div class="flex flex-col sm:gap-4 sm:py-14 sm:pl-64">
 			<header
 				class="sticky top-0 z-30 mb-2 flex h-14 items-center gap-4 border-b bg-background px-4 sm:hidden"
 			>
@@ -121,7 +146,7 @@
 							<span class="sr-only">Toggle Menu</span>
 						</Button>
 					</Sheet.Trigger>
-					<Sheet.Content side="left" class="sm:max-w-xs">
+					<Sheet.Content side="left" class="max-w-64">
 						<nav class="grid gap-6 text-lg font-medium">
 							<DropdownMenu.Root>
 								<DropdownMenu.Trigger asChild let:builder>
@@ -150,13 +175,13 @@
 										><p>Welcome, {$page.data.session?.user?.name}!</p></DropdownMenu.Label
 									>
 									<DropdownMenu.Separator />
-									<a href="/settings">
+									<a href="/app/settings">
 										<DropdownMenu.Item class="cursor-pointer">Settings</DropdownMenu.Item></a
 									>
 									<DropdownMenu.Separator />
 									<DropdownMenu.Item>
 										<SignOut options={{ redirectTo: '/' }}>
-											<div slot="submitButton" class="flex w-48 items-start">Sign out</div>
+											<div slot="submitButton" class="flex w-56 items-start">Sign out</div>
 										</SignOut>
 									</DropdownMenu.Item>
 								</DropdownMenu.Content>
@@ -212,6 +237,25 @@
 						</nav>
 					</Sheet.Content>
 				</Sheet.Root>
+				<div class="relative ml-auto flex-1 md:grow-0"></div>
+				<DropdownMenu.Root>
+					<DropdownMenu.Trigger asChild let:builder>
+						<Button builders={[builder]} variant="outline" size="icon">
+							<Sun
+								class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+							/>
+							<Moon
+								class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+							/>
+							<span class="sr-only">Toggle theme</span>
+						</Button>
+					</DropdownMenu.Trigger>
+					<DropdownMenu.Content align="end">
+						<DropdownMenu.Item on:click={() => setMode('light')}>Light</DropdownMenu.Item>
+						<DropdownMenu.Item on:click={() => setMode('dark')}>Dark</DropdownMenu.Item>
+						<DropdownMenu.Item on:click={() => resetMode()}>System</DropdownMenu.Item>
+					</DropdownMenu.Content>
+				</DropdownMenu.Root>
 			</header>
 			<slot />
 		</div>
