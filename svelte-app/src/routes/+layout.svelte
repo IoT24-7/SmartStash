5b25e6df66e8833de73512da69d8768ad397922b
Angelo Convento <convento.angelo@gmail.com>
2024-05-24 11:20:06 +0800

    chore: Refactor navigation menu styling and layout

--- svelte-app/src/routes/+layout.svelte
+++ svelte-app/src/routes/+layout.svelte
@@ -19,12 +19,12 @@
 {#if $page.data.session?.user && $page.route.id !== '/'}
 	<div class="bg-muted/40 flex min-h-screen w-full flex-col sm:flex-row">
 		<aside class="bg-background fixed inset-y-0 left-0 z-10 hidden w-56 flex-col border-r sm:flex">
-			<nav class="flex flex-col items-start gap-4 px-2 sm:py-5">
+			<nav class="flex flex-col items-start gap-2 px-2 sm:py-5">
 				<DropdownMenu.Root>
 					<DropdownMenu.Trigger asChild let:builder>
 						<Button
 							variant="ghost"
-							class="h-auto w-full items-start justify-start"
+							class="mb-2 h-auto w-full items-start justify-start"
 							builders={[builder]}
 						>
 							<div class="flex flex-row items-center gap-2">
@@ -58,38 +58,56 @@
 						</DropdownMenu.Item>
 					</DropdownMenu.Content>
 				</DropdownMenu.Root>
-				<div class="flex justify-start">
-					<a
-						href="/dashboard"
-						class="text-accent-foreground hover:text-foreground flex items-center gap-4 rounded-lg px-4 transition-colors"
-					>
+				<div
+					class="text-muted-foreground hover:text-foreground hover:bg-accent colors flex w-full justify-start rounded-lg py-2 transition-colors {$page.route.id?.startsWith(
+						'/dashboard'
+					)
+						? 'text-primary hover:text-primary bg-primary/30 hover:bg-primary/30'
+						: ''}"
+				>
+					<a href="/dashboard" class="flex items-center gap-4 px-4">
 						<Home class="h-5 w-5" />
 						<p class="text-lg font-medium">Dashboard</p>
 					</a>
 				</div>
-				<a
-					href="/shopping-list"
-					class="text-muted-foreground hover:text-foreground flex items-center gap-4 rounded-lg px-4 transition-colors"
+				<div
+					class="text-muted-foreground hover:text-foreground hover:bg-accent colors flex w-full justify-start rounded-lg py-2 transition-colors {$page.route.id?.startsWith(
+						'/shopping-list'
+					)
+						? 'text-primary hover:text-primary bg-primary/30 hover:bg-primary/30'
+						: ''}"
 				>
-					<ShoppingCart class="h-5 w-5" />
-					<p class="text-lg font-medium">Shopping List</p>
-				</a>
-				<a
-					href="/notifications"
-					class="text-muted-foreground hover:text-foreground flex items-center gap-4 rounded-lg px-4 transition-colors"
+					<a href="/shopping-list" class="flex items-center gap-4 px-4">
+						<ShoppingCart class="h-5 w-5" />
+						<p class="text-lg font-medium">Shopping List</p>
+					</a>
+				</div>
+				<div
+					class="text-muted-foreground hover:text-foreground hover:bg-accent colors flex w-full justify-start rounded-lg py-2 transition-colors {$page.route.id?.startsWith(
+						'/notifications'
+					)
+						? 'text-primary hover:text-primary bg-primary/30 hover:bg-primary/30'
+						: ''}"
 				>
-					<Bell class="h-5 w-5" />
-					<p class="text-lg font-medium">Notifications</p>
-				</a>
+					<a href="/notifications" class="flex items-center gap-4 px-4">
+						<Bell class="h-5 w-5" />
+						<p class="text-lg font-medium">Notifications</p>
+					</a>
+				</div>
 			</nav>
 			<nav class="mt-auto flex flex-col items-start gap-4 px-2 sm:py-5">
-				<a
-					href="/settings"
-					class="text-muted-foreground hover:text-foreground flex items-center gap-4 rounded-lg px-4 transition-colors"
+				<div
+					class="text-muted-foreground hover:text-foreground hover:bg-accent colors flex w-full justify-start rounded-lg py-2 transition-colors {$page.route.id?.startsWith(
+						'/settings'
+					)
+						? 'text-primary hover:text-primary bg-primary/30 hover:bg-primary/30'
+						: ''}"
 				>
-					<Settings class="h-5 w-5" />
-					<p class="text-lg font-medium">Settings</p>
-				</a>
+					<a href="/settings" class="flex items-center gap-4 px-4">
+						<Settings class="h-5 w-5" />
+						<p class="text-lg font-medium">Settings</p>
+					</a>
+				</div>
 			</nav>
 		</aside>
 		<div class="flex w-full flex-col sm:gap-4 sm:py-14 sm:pl-56">
@@ -109,7 +127,7 @@
 								<DropdownMenu.Trigger asChild let:builder>
 									<Button
 										variant="ghost"
-										class="h-auto w-4/5 items-start justify-start"
+										class="h-auto w-full items-start justify-start"
 										builders={[builder]}
 									>
 										<div class="flex flex-row items-center gap-2">
@@ -143,31 +161,54 @@
 									</DropdownMenu.Item>
 								</DropdownMenu.Content>
 							</DropdownMenu.Root>
-							<a
-								href="/dashboard"
-								class="text-accent-foreground hover:text-foreground flex items-center gap-4 px-2.5"
+							<div
+								class="text-muted-foreground hover:text-foreground hover:bg-accent colors flex w-full justify-start rounded-lg py-2 transition-colors {$page.route.id?.startsWith(
+									'/dashboard'
+								)
+									? 'text-primary hover:text-primary bg-primary/30 hover:bg-primary/30'
+									: ''}"
 							>
-								<Home class="h-5 w-5" />
-								Dashboard
-							</a>
-							<a href="/shopping-list" class="text-muted-foreground flex items-center gap-4 px-2.5">
-								<ShoppingCart class="h-5 w-5" />
-								Shopping List
-							</a>
-							<a
-								href="/notifications"
-								class="text-muted-foreground hover:text-foreground flex items-center gap-4 px-2.5"
+								<a href="/dashboard" class="flex items-center gap-4 px-4">
+									<Home class="h-5 w-5" />
+									<p class="text-lg font-medium">Dashboard</p>
+								</a>
+							</div>
+							<div
+								class="text-muted-foreground hover:text-foreground hover:bg-accent colors flex w-full justify-start rounded-lg py-2 transition-colors {$page.route.id?.startsWith(
+									'/shopping-list'
+								)
+									? 'text-primary hover:text-primary bg-primary/30 hover:bg-primary/30'
+									: ''}"
 							>
-								<Bell class="h-5 w-5" />
-								Notifications
-							</a>
-							<a
-								href="/settings"
-								class="text-muted-foreground hover:text-foreground flex items-center gap-4 px-2.5"
+								<a href="/shopping-list" class="flex items-center gap-4 px-4">
+									<ShoppingCart class="h-5 w-5" />
+									<p class="text-lg font-medium">Shopping List</p>
+								</a>
+							</div>
+							<div
+								class="text-muted-foreground hover:text-foreground hover:bg-accent colors flex w-full justify-start rounded-lg py-2 transition-colors {$page.route.id?.startsWith(
+									'/notifications'
+								)
+									? 'text-primary hover:text-primary bg-primary/30 hover:bg-primary/30'
+									: ''}"
 							>
-								<Settings class="h-5 w-5" />
-								Settings
-							</a>
+								<a href="/notifications" class="flex items-center gap-4 px-4">
+									<Bell class="h-5 w-5" />
+									<p class="text-lg font-medium">Notifications</p>
+								</a>
+							</div>
+							<div
+								class="text-muted-foreground hover:text-foreground hover:bg-accent colors flex w-full justify-start rounded-lg py-2 transition-colors {$page.route.id?.startsWith(
+									'/settings'
+								)
+									? 'text-primary hover:text-primary bg-primary/30 hover:bg-primary/30'
+									: ''}"
+							>
+								<a href="/settings" class="flex items-center gap-4 px-4">
+									<Settings class="h-5 w-5" />
+									<p class="text-lg font-medium">Settings</p>
+								</a>
+							</div>
 						</nav>
 					</Sheet.Content>
 				</Sheet.Root>
