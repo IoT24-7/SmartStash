4a27926b4654500d3d0a1cb28f9bdad6af9a85f5
jrrelucio <jrrelucio@up.edu.ph>
2024-05-28 17:30:52 +0800

    Updated the ino file to communicate with ESP32

--- esp32/esp32_LCDandweight.ino
+++ esp32/esp32_LCDandweight.ino
@@ -2,6 +2,16 @@
 #include <LiquidCrystal_I2C.h>      // for LCD Display 
 #include "HX711.h"                  // for load cell
 
+#include <Arduino.h>
+#if defined(ESP32)
+  #include <WiFi.h>
+#elif defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#endif
+#include <Firebase_ESP_Client.h>
+#include "addons/TokenHelper.h" //Provide the token generation process info.
+#include "addons/RTDBHelper.h" //Provide the RTDB payload printing info and other helper functions.
+
 // HX711 module pins
 const int LOADCELL_DOUT_PIN = 16;
 const int LOADCELL_SCK_PIN = 4;
@@ -17,8 +27,30 @@ HX711 scale;
 // Initialize the LCD object
 LiquidCrystal_I2C lcd(LCD_ADDRESS, LCD_COLUMNS, LCD_ROWS);
 
+// DEFINITIONS FOR FIREBASE
+// Insert your network credentials
+#define WIFI_SSID "4studentstoo"
+#define WIFI_PASSWORD "W1F14students"
+
+// Insert Firebase project API Key
+#define API_KEY "AIzaSyD3qNpR_UAi5sXHIX4Way38C-uELvnxkdk"
+
+// Insert RTDB URLefine the RTDB URL */
+#define DATABASE_URL "https://cs145-smartstash-default-rtdb.asia-southeast1.firebasedatabase.app/" 
+
+//Define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+int count = 0;
+bool signupOK = false;
+
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial.println("HX711 Demo");
 
   // Initialize the HX711 scale
@@ -60,6 +92,39 @@ void setup() {
   // Initialize the LCD
   lcd.init();
   lcd.backlight();
+
+  //Firebase Setup
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Sign up */
+  if (Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("ok");
+    signupOK = true;
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
 }
 
 
@@ -82,4 +147,19 @@ void loop() {
   lcd.setCursor(0, 1);
   lcd.print(scale.get_units(), 1); // displays up to one decimal 
   lcd.print(" g");
+
+  //Updating firebase every interval
+  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
+    sendDataPrevMillis = millis();
+    // Write the current reading on the database path testData2/container1/currentWeight
+    if (Firebase.RTDB.setFloat(&fbdo, "testData2/container1/currentWeight", scale.get_units())){
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+  }
 }
