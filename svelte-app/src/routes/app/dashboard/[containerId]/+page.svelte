eac24fe589e664dc57184ff1fbf49b92495b7478
Angelo Convento <convento.angelo@gmail.com>
2024-05-28 17:57:49 +0800

    feat: Add new page for container details in dashboard

---
+++ svelte-app/src/routes/app/dashboard/[containerId]/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+// @ts-nocheck
+
+	export let data;
+	console.log(data);
+	const containerData = data.container;
+
+	import { mediaQuery } from 'svelte-legos';
+	let open = false;
+	const isDesktop = mediaQuery('(min-width: 640px)');
+</script>
+
+<div class="relative flex h-full w-full flex-col">
+	<main class="flex flex-col gap-2 px-5">
+		<h2 class="scroll-m-20 py-4 text-3xl font-extrabold tracking-tight sm:mt-3">
+			{containerData.foodName}
+		</h2>
+	</main>
+</div>
