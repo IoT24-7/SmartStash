f003a6dace584c4fe3e9f2c79ccb64f5c3481955
Caleb Bunye <jebunye@up.edu.ph>
2024-05-29 22:58:00 +0800

    feat(backend):FCM integration

--- svelte-app/static/firebase-messaging-sw.js
+++ svelte-app/static/firebase-messaging-sw.js
@@ -1,18 +1,28 @@
 // Give the service worker access to Firebase Messaging.
-self.importScripts('https://www.gstatic.com/firebasejs/7.2.1/firebase-app.js');
-self.importScripts('https://www.gstatic.com/firebasejs/7.2.1/firebase-messaging.js');
+self.importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+self.importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
-self.firebase.initializeApp({
-	messagingSenderId: '985579041843'
-});
+const firebaseConfig = {
+	apiKey: 'AIzaSyD3qNpR_UAi5sXHIX4Way38C-uELvnxkdk',
+	authDomain: 'cs145-smartstash.firebaseapp.com',
+	databaseURL: 'https://cs145-smartstash-default-rtdb.asia-southeast1.firebasedatabase.app',
+	projectId: 'cs145-smartstash',
+	storageBucket: 'cs145-smartstash.appspot.com',
+	messagingSenderId: '985579041843',
+	appId: '1:985579041843:web:82d802b451827653e99f46',
+	measurementId: 'G-3HQ89HFLNS'
+};
+
+self.firebase.initializeApp(firebaseConfig);
 const messaging = self.firebase.messaging();
 
-messaging.setBackgroundMessageHandler((payload) => {
-	const notification = JSON.parse(payload.data.notification);
-	const notificationTitle = notification.title;
+messaging.onBackgroundMessage((payload) => {
+	console.log('Received background message ', payload);
+
+	const notificationTitle = payload.notification.title;
 	const notificationOptions = {
-		body: notification.body
+		body: payload.notification.body
 	};
 
-	return self.registration.showNotification(notificationTitle, notificationOptions);
+	self.registration.showNotification(notificationTitle, notificationOptions);
 });
