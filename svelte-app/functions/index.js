38c271931c50f5ae3150e9748963b526f6d7b923
jcmaep <jcmaep@gmail.com>
2024-05-30 12:44:48 +0800

    Update index.js

--- svelte-app/functions/index.js
+++ svelte-app/functions/index.js
@@ -116,3 +116,55 @@ exports.syncfoodNameToRTDB = functions.firestore
         console.error("Error updating RTDB: ", error);
     }
 });
+
+// Sends a notifications to all users when a new message is posted.
+exports.sendNotifications = functions.firestore.document('users/{userID}/notifications/{notificationID}').onCreate(
+    async (snapshot) => {
+      // Notification details.
+
+      const notificationData = snap.data();
+
+      const title = notificationData.foodItem;
+      const body = `You're running low on ${notificationData.foodItem}.`;
+
+      const payload = {
+        notification: {
+            title: title,
+            body: body,
+            //click_action: "https://yourapp.firebaseapp.com"
+        }
+      };
+  
+      // Get the list of device tokens.
+      const allTokens = await admin.firestore().collection('fcmTokens').get();
+      const tokens = [];
+      allTokens.forEach((tokenDoc) => {
+        tokens.push(tokenDoc.id);
+      });
+  
+      if (tokens.length > 0) {
+        // Send notifications to all tokens.
+        const response = await admin.messaging().sendToDevice(tokens, payload);
+        await cleanupTokens(response, tokens);
+        functions.logger.log('Notifications have been sent and tokens cleaned up.');
+      }
+    });
+
+// Cleans up the tokens that are no longer valid.
+function cleanupTokens(response, tokens) {
+    // For each notification we check if there was an error.
+    const tokensDelete = [];
+    response.results.forEach((result, index) => {
+      const error = result.error;
+      if (error) {
+        functions.logger.error('Failure sending notification to', tokens[index], error);
+        // Cleanup the tokens that are not registered anymore.
+        if (error.code === 'messaging/invalid-registration-token' ||
+            error.code === 'messaging/registration-token-not-registered') {
+          const deleteTask = admin.firestore().collection('fcmTokens').doc(tokens[index]).delete();
+          tokensDelete.push(deleteTask);
+        }
+      }
+    });
+    return Promise.all(tokensDelete);
+   }
